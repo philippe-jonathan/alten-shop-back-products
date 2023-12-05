4fbefcd6bb788fa47c3dc889b5c26f6970c10bbf
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-12-05 16:43:24 +0100

    add init.sql

---
+++ back/Database/init.sql
@@ -0,0 +1,22 @@
+USE alten_shop;
+
+CREATE TABLE produits (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    code VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL,
+    inventory_status ENUM('INSTOCK', 'OUTOFSTOCK') NOT NULL,
+    category_id INT NOT NULL,
+    image VARCHAR(255),
+    rating INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
