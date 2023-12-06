eb39a3ce56de59530b3bc166da8f01746dcfbc5f
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-12-06 13:40:39 +0100

    Connection to database + healthcheck

--- back/app/index.js
+++ back/app/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,22 @@ app.get('/', (req, res) => {
     res.send('Works!');
 });
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-  });
+// Configuration de la base de données
+const database = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+});
+
+database.connect((err) => {
+    if(err) {
+        console.error('Error connecting to the database :', err);
+    } else {
+        console.log('Connected to the database');
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`);
+        });
+    }
+});
+
